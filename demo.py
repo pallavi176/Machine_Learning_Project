8b527155b88386853c3a43b125bc92338655eced
pallavi.saxena <saxena.pallavi7211@gmail.com>
2022-08-29 22:09:53 +0530

    eda added

--- demo.py
+++ demo.py
@@ -5,10 +5,12 @@ from housing.logger import logging
 
 def main():
     try:
-        pipeline = Pipeline()
-        pipeline.run_pipeline()
+        # pipeline = Pipeline()
+        # pipeline.run_pipeline()
         # data_validation_config = Configuration().get_data_validation_config()
         # print(data_validation_config)
+        data_transformation_config = Configuration().get_data_transformation_config()
+        print(data_transformation_config)
     except Exception as e:
         logging.error(f"{e}")
         print(e)
