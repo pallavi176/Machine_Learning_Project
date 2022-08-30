662893aac8d624f31536f0f51501a085e7f06422
pallavi.saxena <saxena.pallavi7211@gmail.com>
2022-08-30 20:29:33 +0530

    Data Transformation completed

--- demo.py
+++ demo.py
@@ -1,16 +1,23 @@
+
 from housing.pipeline.pipeline import Pipeline
 from housing.config.configuration import Configuration
 from housing.exception import HousingException
 from housing.logger import logging
+from housing.component.data_transformation import DataTransformation
 
 def main():
     try:
-        # pipeline = Pipeline()
-        # pipeline.run_pipeline()
+        pipeline = Pipeline()
+        pipeline.run_pipeline()
         # data_validation_config = Configuration().get_data_validation_config()
         # print(data_validation_config)
-        data_transformation_config = Configuration().get_data_transformation_config()
-        print(data_transformation_config)
+        # data_transformation_config = Configuration().get_data_transformation_config()
+        # print(data_transformation_config)
+        # schema_file_path = r"\config\schema.yaml"
+        # file_path = r"\housing\artifact\data_ingestion\2022-08-29-16-23-20\ingested_data\train\housing.csv"
+        # df = DataTransformation.load_data(file_path=file_path, schema_file_path=schema_file_path)
+        # print(df.columns)
+        # print(df.dtypes)
     except Exception as e:
         logging.error(f"{e}")
         print(e)
