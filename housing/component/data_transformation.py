5fd97f786f3d9c6fa1b961c70073468f9a97fc19
pallavi.saxena <saxena.pallavi7211@gmail.com>
2022-08-30 23:23:20 +0530

    model factory housing predictor

--- housing/component/data_transformation.py
+++ housing/component/data_transformation.py
@@ -147,7 +147,7 @@ class DataTransformation:
             logging.info(f"Splitting input and target feature from training and testing dataframe.")
             input_feature_train_df = train_df.drop(columns = [target_column_name], axis = 1)
             target_feature_train_df = train_df[target_column_name]
-            input_feature_test_df = test_df.drop(columns=[target_column_name], axis = 1)
+            input_feature_test_df = test_df.drop(columns = [target_column_name], axis = 1)
             target_feature_test_df = test_df[target_column_name]          
             logging.info(f"Applying preprocessing object on training dataframe and testing dataframe")
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
