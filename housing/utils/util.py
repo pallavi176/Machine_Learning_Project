8c9192cf0f982c52011011c6bef952ea8639ba39
pallavi.saxena <saxena.pallavi7211@gmail.com>
2022-08-31 21:44:51 +0530

    Model Evaluation done

--- housing/utils/util.py
+++ housing/utils/util.py
@@ -18,6 +18,20 @@ def read_yaml_file(file_path:str)->dict:
     except Exception as e:
         raise HousingException(e, sys) from e
 
+def write_yaml_file(file_path:str,data:dict=None):
+    """
+    Create yaml file 
+    file_path: str
+    data: dict
+    """
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path,"w") as yaml_file:
+            if data is not None:
+                yaml.dump(data,yaml_file)
+    except Exception as e:
+        raise HousingException(e,sys)
+
 def save_numpy_array_data(file_path: str, array: np.array):
     """
     Save numpy array data to file
